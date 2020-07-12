cd61dfd239ae5d24a7b55d279e57eb7d5897d750
Rishikesh <rishikesh1419@gmail.com>
2020-07-12 21:34:50 +0530

    Imported data into db

--- backend_API_server/data_import.py
+++ backend_API_server/data_import.py
@@ -1,3 +1,21 @@
 import pymongo
+import json
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+print(myclient.list_database_names())
+
+mydb = myclient["potterdb"]
+
+# coll = mydb["characters"]
+# coll = mydb["houses"]
+coll = mydb["spells"]
+
+# with open("HP_Data/characters.json") as file:
+# with open("HP_Data/houses.json") as file:
+with open("HP_Data/spells.json") as file:
+    file_data = json.load(file) 
+ 
+if isinstance(file_data, list): 
+    coll.insert_many(file_data)   
+else: 
+    coll.insert_one(file_data) 
