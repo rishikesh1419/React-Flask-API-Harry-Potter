85c17a32e22338e24b5d8de0726943559ec0b8b7
Rishikesh Kadam <rishikesh1419@gmail.com>
2021-05-05 14:07:34 +0530

    Cleanup

--- backend_API_server/api_server.py
+++ backend_API_server/api_server.py
@@ -15,16 +15,18 @@ Get a random house
 Returns: a random house with its id, name, mascot, head of house, house ghost, founder, school,
          list of members (id), list of values, list of colors
 """
+
+
 @app.route("/sortinghat", methods=["GET"])
-def sorting_hat() :
-    houses = ['Gryffindor', 'Ravenclaw', 'Slytherin', 'Hufflepuff']
-    try :
-        house = dumps(mydb.houses.find({"name" : houses[randint(0,3)]}))
-        if len(house) > 0 :
+def sorting_hat():
+    houses = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
+    try:
+        house = dumps(mydb.houses.find({"name": houses[randint(0, 3)]}))
+        if len(house) > 0:
             return house
-        else :
+        else:
             return "", 404
-    except :
+    except:
         return "", 500
 
 
@@ -33,80 +35,102 @@ Characters request route to get all characters and to search for characters by n
 Valid uses: /characters (Get all characters), /characters?name=search_string (Search for a character)
 Returns: character's _id, name, house and species
 """
+
+
 @app.route("/characters", methods=["GET"])
-def get_chars() :
-    try :
+def get_chars():
+    try:
         params = parse_query(request.query_string)
-        if params :
-            characters = dumps(mydb.characters.find({"name" : {"$regex" : params["name"], "$options" : "i"}}, {"name":1, "house": 1, "species": 1}))
-            if len(characters) > 0 :
+        if params:
+            characters = dumps(
+                mydb.characters.find(
+                    {"name": {"$regex": params["name"], "$options": "i"}},
+                    {"name": 1, "house": 1, "species": 1},
+                )
+            )
+            if len(characters) > 0:
                 return characters
-            else :
+            else:
                 return "", 404
-        else :
-            if mydb.characters.count_documents({}) > 0 :
-                return dumps(mydb.characters.find({}, {"name": 1, "house": 1, "species": 1}))
-            else :
+        else:
+            if mydb.characters.count_documents({}) > 0:
+                return dumps(
+                    mydb.characters.find({}, {"name": 1, "house": 1, "species": 1})
+                )
+            else:
                 return jsonify([])
-    except :
+    except:
         return "", 500
+
 
 """
 Get a character using _id
 Returns: _id, name, role, wand, boggart, patronus, house, school, alias, animagus, blood status, species,
           if member of ministry of magic, order of phoenix, Dumbledore's army, death eaters (4 booleans)
 """
+
+
 @app.route("/characters/<id>", methods=["GET"])
-def get_char_details(id) :
-    try :
-        characters = dumps(mydb.characters.find({"_id" : id}))
-        if len(characters) > 0 :
+def get_char_de tails(id):
+    try:
+        characters = dumps(mydb.characters.find({"_id": id}))
+        if len(characters) > 0:
             return characters
-        else :
+        else:
             return "", 404
-    except :
+    except:
         return "", 500
+
 
 """
 Get house names with _id and their founders
 """
+
+
 @app.route("/houses", methods=["GET"])
-def get_houses() :
-    try :
-        if mydb.houses.count_documents({}) > 0 :
-            return dumps(mydb.houses.find({}, {"name":1, "founder":1}))
-        else :
+def get_houses():
+    try:
+        if mydb.houses.count_documents({}) > 0:
+            return dumps(mydb.houses.find({}, {"name": 1, "founder": 1}))
+        else:
             return jsonify([])
-    except :
+    except:
         return "", 500
+
 
 """
 Get house details using name (Gryffindor, Slytherin, Ravenclaw, Hufflepuff)
 Returns: _id, name, mascot, head of house, house ghost, founder, school,
          list of members (_id), list of values, list of colors
 """
+
+
 @app.route("/houses/<name>", methods=["GET"])
-def get_house_details(name) :
-    try :
-        if mydb.houses.count_documents({}) > 0 :
+def get_house_details(name):
+    try:
+        if mydb.houses.count_documents({}) > 0:
             return dumps(mydb.houses.find({"name": name}))
-        else :
+        else:
             return jsonify([])
-    except :
+    except:
         return "", 500
+
 
 """
 Return all spells with details: _id, spell, type, effect
 """
+
+
 @app.route("/spells", methods=["GET"])
-def get_spells() :
-    try :
-        if mydb.spells.count_documents({}) > 0 :
+def get_spells():
+    try:
+        if mydb.spells.count_documents({}) > 0:
             return dumps(mydb.spells.find())
-        else :
+        else:
             return jsonify([])
-    except :
+    except:
         return "", 500
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- backend_API_server/data_import.py
+++ backend_API_server/data_import.py
@@ -2,19 +2,14 @@ import pymongo
 import json
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
 mydb = myclient["potterdb"]
+files = ["characters", "houses", "spells"]
 
-# coll = mydb["characters"]
-# coll = mydb["houses"]
-coll = mydb["spells"]
-
-# with open("HP_Data/characters.json") as file:
-# with open("HP_Data/houses.json") as file:
-with open("HP_Data/spells.json") as file:
-    file_data = json.load(file) 
- 
-if isinstance(file_data, list): 
-    coll.insert_many(file_data)   
-else: 
-    coll.insert_one(file_data) 
+for f in files:
+    coll = mydb[f]
+    with open(f"HP_Data/{f}.json") as file:
+        file_data = json.load(file)
+    if isinstance(file_data, list):
+        coll.insert_many(file_data)
+    else:
+        coll.insert_one(file_data)
